4bef0a6f770a36016f86af09d0ff8c96db4121fe
22091397049 Septian Nur <114415096+SeptianNurIkhsan@users.noreply.github.com>
2024-03-30 09:37:59 +0700

    Models

---
+++ User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasFactory, HasApiTokens;
+
+    protected $table = "users";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $timestamps = true;
+    public $incrementing = true;
+    protected $fillable = [
+        'username',
+        'password',
+        'name'
+    ];
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class, 'user_id', 'id');
+    }
+}
